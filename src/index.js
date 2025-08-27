853272afb15bc780d627c20240e4a8f607157cca
Thrapis <50047219+Thrapis@users.noreply.github.com>
2025-08-27 13:06:20 +0300

    feat: add implementation of bracket check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const chars = str.split('');
+
+  for (let j = 0; j < chars.length; j += 1) {
+    const e = chars[j];
+
+    for (let i = 0; i < bracketsConfig.length; i += 1) {
+      if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+        if (bracketsConfig[i][0] === e) {
+          if (stack.length > 0 && stack[stack.length - 1] === e) {
+            stack.pop();
+          } else {
+            stack.push(e);
+          }
+        }
+      } else {
+        if (bracketsConfig[i][0] === e) {
+          stack.push(e);
+          break;
+        }
+        if (bracketsConfig[i][1] === e) {
+          if (stack.pop() !== bracketsConfig[i][0]) {
+            return false;
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
